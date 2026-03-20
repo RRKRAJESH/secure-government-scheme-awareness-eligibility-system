d4e72664dee2e0e4de085e2990ed4cfb24f191ca
RAJESH KUMAR R <kumarrajesh984000@gmail.com>
2026-03-20 21:55:35 +0530

    dfd,wfd and er diagram were added and remove the tamilnadu symbol backaground

--- front_end/login-ui/src/pages/MainLayout.jsx
+++ front_end/login-ui/src/pages/MainLayout.jsx
@@ -1,12 +1,8 @@
 import "../styles/layout.css";
-import watermarkImage from "../assets/TamilNadu_Seal_With_English_Caption.svg.png";
 
 function MainLayout({ children }) {
   return (
     <div className="app-bg">
-      <div className="watermark-container">
-        <img src={watermarkImage} alt="" className="watermark-image" />
-      </div>
       {children}
     </div>
   );
